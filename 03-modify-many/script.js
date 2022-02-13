0bfb9a340ee5fa657e61440de60a6360b5eebdab
Min Qi Neo <neominqi@hotmail.com>
2022-02-13 15:11:15 +0800

    created 03-modify-many

---
+++ 03-modify-many/script.js
@@ -0,0 +1,10 @@
+let r = document.querySelectorAll('.recommended');
+for (let eachLi of r) {
+    eachLi.style.backgroundColor = 'green';
+}
+/*
+for (let i =0; i < r.length; i++) {
+    r[i].style.backgroundColor = 'green'
+}
+
+*/
